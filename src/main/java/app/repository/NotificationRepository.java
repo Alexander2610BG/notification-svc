567ca26fff3d51350abd0bdd1372ee146275fabf
Alexandear2610BG <aleksandurskachkov@gmail.com>
2025-03-21 17:09:10 +0200

    Repaired NotificationRepository

--- src/main/java/app/repository/NotificationRepository.java
+++ src/main/java/app/repository/NotificationRepository.java
@@ -2,6 +2,7 @@ package app.repository;
 
 import app.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    @Query("""
+        SELECT n FROM Notification n WHERE n.userId = :userId AND n.isDeleted = false
+    """)
     List<Notification> findAllByUserIdAndDeletedIsFalse(UUID userId);
 }
